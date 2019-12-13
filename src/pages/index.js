0707665a3ac503d7235c5ea56a5f0299aa3dd86b
Alberto Prado <albertoprado.desarrollador@gmail.com>
2019-12-13 09:49:50 -0600

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,23 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+
+    <section>
+      <h2> Section 1 </h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur gravida turpis id luctus posuere. Duis a tempor mauris. Ut sollicitudin et velit sed euismod. Sed tempor cursus erat, ut accumsan massa faucibus non. Nulla facilisi. Suspendisse potenti. Nullam vitae lectus felis. Vivamus tempor eget massa in mattis. Nulla pharetra, risus vitae scelerisque hendrerit, mauris nunc faucibus massa, in tincidunt felis tellus at nulla. Sed rutrum, urna et porttitor accumsan, lectus eros volutpat lacus, nec consectetur mi arcu quis diam. Integer lobortis nibh nec felis efficitur hendrerit. </p>
+    </section>
+
+    <section>
+    <h2> Section 2 </h2>
+      <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur gravida turpis id luctus posuere. Duis a tempor mauris. Ut sollicitudin et velit sed euismod. Sed tempor cursus erat, ut accumsan massa faucibus non. Nulla facilisi. Suspendisse potenti. Nullam vitae lectus felis. Vivamus tempor eget massa in mattis. Nulla pharetra, risus vitae scelerisque hendrerit, mauris nunc faucibus massa, in tincidunt felis tellus at nulla. Sed rutrum, urna et porttitor accumsan, lectus eros volutpat lacus, nec consectetur mi arcu quis diam. Integer lobortis nibh nec felis efficitur hendrerit. </p>
+      <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/iMacxZQMPXs?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </section>
+
+    <section>
+      <h2> Section 3 </h2>
+      
+    </section>
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
